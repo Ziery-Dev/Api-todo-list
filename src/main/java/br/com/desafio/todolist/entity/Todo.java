039680935defa5d0e845d770568aa94eca102c32
Ziery-Dev <zieryDev@gmail.com>
2024-10-28 20:26:58 -0300

    teste de sucesso implementado

--- src/main/java/br/com/desafio/todolist/entity/Todo.java
+++ src/main/java/br/com/desafio/todolist/entity/Todo.java
@@ -14,6 +14,14 @@ public class Todo{
     private boolean realizado;
     private int prioridade;
 
+    public Todo( String nome, String descricao, boolean realizado, int prioridade) {
+
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
+
     public String getNome() {
         return nome;
     }
--- src/test/java/br/com/desafio/todolist/TodolistApplicationTests.java
+++ src/test/java/br/com/desafio/todolist/TodolistApplicationTests.java
@@ -1,13 +1,41 @@
 package br.com.desafio.todolist;
 
+import br.com.desafio.todolist.entity.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class TodolistApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
+	//teste de sucesso
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo 1", "desc todo 1", false, 1);
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+				.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+				.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+				.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+
+
+
+
+
 	}
 
 }
