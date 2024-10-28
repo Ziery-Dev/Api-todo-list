833e690061635c64cdd451be5d1f064a97d7ff3f
Ziery-Dev <zieryDev@gmail.com>
2024-10-27 22:19:39 -0300

    Classe service criada

--- src/main/java/br/com/desafio/todolist/service/TodoService.java
+++ src/main/java/br/com/desafio/todolist/service/TodoService.java
@@ -1,4 +1,46 @@
 package br.com.desafio.todolist.service;
 
+import br.com.desafio.todolist.entity.Todo;
+import br.com.desafio.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TodoService {
+
+    @Autowired
+    TodoRepository acao; //injeção de dependencia da classe repositorio
+    @Autowired
+    private TodoRepository todoRepository;
+
+
+    //Por se tratar de um TodoList, todos o métodos retornam a lista de tarefas
+
+    //método que cria uma tarefa
+    public List<Todo>creat(Todo obj){
+        acao.save(obj);
+        return list();
+    }
+
+    //Método de consulta que retorna na ordem de prioridade e depois por nome
+    public  List <Todo> list(){
+        Sort sort =  Sort.by("prioridade").descending().and(Sort.by("nome").ascending()); //ordenação do que irá ser exibido primeiro
+       return todoRepository.findAll(sort);
+    }
+
+    //atualizar uma tarefa
+    public  List <Todo> update(Todo obj){
+        acao.save(obj);
+        return list();
+    }
+
+    //remover uma tarefa
+    public  List<Todo> delete(Long id){
+        acao.deleteById(id);
+        return list();
+    }
+
 }
